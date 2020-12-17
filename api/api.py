41e07f9a25f55a92a903552ed0bb2fdfe4ec8e92
DevDaveFrame <dframe22@gmail.com>
2020-12-16 21:58:24 -0500

    added methods Employee

--- api/api.py
+++ api/api.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/employees')
-def get_employees():
+@app.route('/employees', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def _employees():
   return {employees = employees.get}
-  
